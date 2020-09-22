f27a99b3dcdd7e0d3414daa33d765f0cf11f317e
Emilio Gozo <emil.gozo@gmail.com>
2020-09-22 23:14:22 +0800

    Updates

---
+++ precache-manifest.4b30ecdb734bb0a359d6b1f734947dd0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21e6626a4352f7b5a0109d8f9e2caad2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fdc4ba7a7f312fca62d",
+    "url": "/static/css/main.c9e370db.chunk.css"
+  },
+  {
+    "revision": "dfdc1eec2ec87885b8cf",
+    "url": "/static/js/2.82c6f3a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.82c6f3a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fdc4ba7a7f312fca62d",
+    "url": "/static/js/main.979107d5.chunk.js"
+  },
+  {
+    "revision": "7a7263076890c3f41da0",
+    "url": "/static/js/runtime-main.e3d89624.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  }
+]);
